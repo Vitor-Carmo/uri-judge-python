42eaef1aa1cdba2c39017d01f2dbbfb923798739
Vitor-Carmo <Vitor-Carmo>
2020-03-31 02:07:49 -0300

    1072 solved

---
+++ Uri_1072.py
@@ -0,0 +1,15 @@
+repeat = int(input())
+
+iin  = out = 0
+
+for i in range(repeat):
+    
+    x = int(input())
+
+    if 10 <= x <= 20: 
+        iin += 1
+    else:
+        out += 1
+
+print('{} in'.format(iin))
+print('{} out'.format(out))
