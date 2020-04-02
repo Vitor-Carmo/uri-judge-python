f5d551781851f69e1796f15cc11cac1ea5d855ca
Vitor Carmo <vitorv0071@gmail.com>
2020-04-02 00:33:33 -0300

    1253 solved (this solution is better)

--- Uri_1253.py
+++ Uri_1253.py
@@ -1,28 +1,18 @@
-alphabet =[ 
-    'A','B','C','D','E','F','G','H','I','J','K','L','M','N','0'
-    ,'P','Q','R','S','T','U','V','W','X','Y','Z'
-]
-
 tests = int(input())
 
 for test in range(tests):
-    
-    code = input()
+
+    code = input().upper()
     key = int(input())
 
+    
     decode = str()
-    if not key == 0:
 
-        for string in code:
-            for i in range(len(alphabet)):
-                if alphabet[i] == string:
-                    if  i - key < 0:
-                        pos = i - key
-                        pos = len(alphabet) + pos
-                    else:
-                        pos = i - key
-                    
-                    decode += alphabet[pos]
-    else:
-        decode = code
+    for l in code:
+        pos = ord(l)-key
+
+        if(pos < 65):
+            decode += chr(91-(65-pos))
+        else:
+            decode += chr(ord(l)-key)
     print(decode)
