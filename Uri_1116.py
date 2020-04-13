e49f1737fbf31f8591d33c3c7a039bf3ab95bdc5
Vitor Carmo <vitorv0071@gmail.com>
2020-04-13 16:26:24 -0300

    1116 solved

---
+++ Uri_1116.py
@@ -0,0 +1,8 @@
+#it's just a little joke LOL,PLEASE, don't do that on your code!
+ijibcz = int(input()) 
+for dnjklao in range(ijibcz):
+    jsdgkç, jasfnjv  = map(int, input().split())
+    if jasfnjv == 0:
+        print('divisao impossivel')
+    else:
+        print('{:.1f}'.format(jsdgkç / jasfnjv))
