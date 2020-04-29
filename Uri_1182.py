e95ce21c61b3440e7384c7b76722462ae96488d3
Vitor Carmo <vitorv0071@gmail.com>
2020-04-29 00:04:55 -0300

    1182 solved

---
+++ Uri_1182.py
@@ -0,0 +1,14 @@
+line = int(input())
+operation = input()   
+sum = 0
+
+for i in range(12):
+    for j in range(12):
+        value = float(input())
+        if j == line:
+            sum += value
+    
+if operation == 'S':
+    print("%.1f" %sum)
+else:
+    print("%.1f" %(sum/12))
