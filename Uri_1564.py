f8dc007421e8b1fc5a0f16d0dfe733b59b88da94
Vitor Carmo <vitorv0071@gmail.com>
2020-05-04 22:16:15 -0300

    1564 solved

---
+++ Uri_1564.py
@@ -0,0 +1,12 @@
+while True:
+    try:
+
+        x = int(input())
+        if not x == 0:
+            print('vai ter duas!')
+        else:
+            print('vai ter copa!')
+
+    except:
+        
+        break
